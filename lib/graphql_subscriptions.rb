5dfe861ffc857c6a8362daf589fe6f1c7a02ef6d
psguazz <p.sguazz@gmail.com>
2024-01-22 21:05:12 +0100

    ...So we can have an actual class with a decent name

--- lib/graphql_subscriptions.rb
+++ lib/graphql_subscriptions.rb
@@ -8,74 +8,76 @@ require "json"
 require_relative "graphql_subscriptions/version"
 
 module GraphqlSubscriptions
-  attr_reader :url, :thread
-  attr_reader :subscription_query
+  class Client
+    attr_reader :url, :thread
+    attr_reader :subscription_query
 
-  def initialize(url, subscription_query:, protocol: "graphql-ws", &message_callback)
-    uri = URI.parse(url)
+    def initialize(url, subscription_query:, protocol: "graphql-ws", &message_callback)
+      uri = URI.parse(url)
 
-    @url = url
-    @subscription_query = subscription_query
-    @message_callback = message_callback
+      @url = url
+      @subscription_query = subscription_query
+      @message_callback = message_callback
 
-    @tcp = TCPSocket.new(uri.host, uri.port)
-    @dead = false
+      @tcp = TCPSocket.new(uri.host, uri.port)
+      @dead = false
 
-    @driver = WebSocket::Driver.client(self, protocols: [protocol])
-    @driver.add_extension(PermessageDeflate)
+      @driver = WebSocket::Driver.client(self, protocols: [protocol])
+      @driver.add_extension(PermessageDeflate)
 
-    @driver.on(:open) do |event|
-      send(JSON.dump(init))
-    end
-
-    @driver.on(:message) do |event|
-      json = JSON.parse(event.data)
-
-      case json["type"]
-      when "connection_ack"
-        send(JSON.dump(subscription))
-      when "data"
-        @message_callback.call(json["payload"]["data"])
+      @driver.on(:open) do |event|
+        send(JSON.dump(init))
       end
+
+      @driver.on(:message) do |event|
+        json = JSON.parse(event.data)
+
+        case json["type"]
+        when "connection_ack"
+          send(JSON.dump(subscription))
+        when "data"
+          @message_callback.call(json["payload"]["data"])
+        end
+      end
+
+      @driver.on(:close) do |event|
+        finalize(event)
+      end
+
+      @thread = Thread.new do
+        @driver.parse(@tcp.read(1)) until @dead
+      end
+
+      @driver.start
     end
 
-    @driver.on(:close) do |event|
-      finalize(event)
+    def send(message)
+      @driver.text(message)
     end
 
-    @thread = Thread.new do
-      @driver.parse(@tcp.read(1)) until @dead
+    def write(data)
+      @tcp.write(data)
     end
 
-    @driver.start
-  end
+    def close
+      @driver.close
+    end
 
-  def send(message)
-    @driver.text(message)
-  end
+    def init
+      {type: "connection_init", payload: {}}
+    end
 
-  def write(data)
-    @tcp.write(data)
-  end
+    def subscription
+      {
+        id: SecureRandom.hex(10), # FIXME: MIGHT CAUSE MULTIPLE MESSAGES
+        type: "start",
+        payload: {query: subscription_query}
+      }
+    end
 
-  def close
-    @driver.close
-  end
-
-  def init
-    {type: "connection_init", payload: {}}
-  end
-
-  def subscription
-    {
-      id: SecureRandom.hex(10), # FIXME: MIGHT CAUSE MULTIPLE MESSAGES
-      type: "start",
-      payload: {query: subscription_query}
-    }
-  end
-
-  def finalize(event)
-    @dead = true
-    @thread.kill
+    def finalize(event)
+      @dead = true
+      @thread.kill
+    end
   end
 end
