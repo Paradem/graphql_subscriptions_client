643311d04f71248cc6aca17a434823d9cffe067d
psguazz <p.sguazz@gmail.com>
2024-01-22 20:40:02 +0100

    Initial commit

---
+++ lib/graphql_subscriptions_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "graphql_subscriptions_client/version"
+
+module GraphqlSubscriptionsClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
